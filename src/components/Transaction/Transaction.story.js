df04d2fc5f8b80901bf60312efe8b4f5ad353a72
Paul Sandwell <paul.sandwell@mirumagency.com>
2018-02-12 16:02:20 +0000

    Using correct propType for amount prop in Transaction story

--- src/components/Transaction/Transaction.story.js
+++ src/components/Transaction/Transaction.story.js
@@ -11,7 +11,7 @@ storiesOf('Transaction')
 		<Transaction
 			from={"0x52903256dd18d85c2dc4a6c999907c9793ea61e3"}
 			to={'0x519475b31653e46d20cd09f9fdcf3b12bdacb4f5'}
-			amount={'1.2'}
+			amount={1.2}
 			timeStamp={'12/02/2018 11:51'}
 			trust={4}
 			transactionHash={'0x2d9c28aefc4768aa0710391d1723743358ba0f1101d443478826e382edb74612'}
