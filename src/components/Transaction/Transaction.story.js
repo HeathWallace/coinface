42320d273bdd2d54102924a2ee75a6ebd0dc4217
Paul Sandwell <paul.sandwell@mirumagency.com>
2018-02-13 16:52:00 +0000

    Fixing eslint errors

--- src/components/Transaction/Transaction.story.js
+++ src/components/Transaction/Transaction.story.js
@@ -9,7 +9,7 @@ storiesOf('Transaction')
 
 	.add('with junk data', () => (
 		<Transaction
-			from={"0x52903256dd18d85c2dc4a6c999907c9793ea61e3"}
+			from={'0x52903256dd18d85c2dc4a6c999907c9793ea61e3'}
 			to={'0x519475b31653e46d20cd09f9fdcf3b12bdacb4f5'}
 			amount={1.2}
 			timeStamp={'12/02/2018 11:51'}
--- src/components/TransactionsList/TransactionsList.story.js
+++ src/components/TransactionsList/TransactionsList.story.js
@@ -15,7 +15,7 @@ storiesOf('TransactionsList')
 		<TransactionsList transactions={
 			[
 				{
-					from: "0x52903256dd18d85c2dc4a6c999907c9793ea61e3",
+					from: '0x52903256dd18d85c2dc4a6c999907c9793ea61e3',
 					to: '0x519475b31653e46d20cd09f9fdcf3b12bdacb4f5',
 					amount: 1.2,
 					timeStamp: '12/02/2018 11:51',
@@ -23,13 +23,13 @@ storiesOf('TransactionsList')
 					transactionHash: '0x2d9c28aefc4768aa0710391d1723743358ba0f1101d443478826e382edb74612',
 				},
 				{
-					from: "0.740338835632784",
+					from: '0.740338835632784',
 					to: '0.21455781911656757',
 					amount: 2.3,
 					timeStamp: '12/02/2018 15:33',
 					trust: 2,
 					transactionHash: '0.14801239520291642',
-				}
+				},
 			]
 		} />
 	));
