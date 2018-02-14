e50c549003b2bca02122c342d58abb1a42541ca1
Gary Purbrick <gary.purbrick@heathwallace.com>
2018-02-14 18:35:36 +0000

    Adding variants of SkypeProfile component

---
+++ src/components/SkypeProfile/SkypeProfile.story.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf, withInfo } from '../../stories';
+
+import SkypeProfile from './SkypeProfile';
+
+storiesOf('SkypeProfile')
+
+	.addDecorator((story, context) => withInfo(SkypeProfile.description)(story)(context))
+
+	.add('known user', () => (
+		<SkypeProfile
+			username='gary_purbrick'
+		/>
+	))
+
+	.add('unknown user', () => (
+		<SkypeProfile
+			username='giohsdhsn'
+		/>
+	))
+
+	.add('empty user', () => (
+		<SkypeProfile	/>
+	));
