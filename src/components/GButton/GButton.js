2eea92cc8cdae7d703fe00e0f936f3a109649656
George <george.bryant@heathwallace.com>
2018-02-14 18:58:49 +0000

    Imported and updated CSS

--- src/components/GButton/GButton.js
+++ src/components/GButton/GButton.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './GButton.css';
+
+//Declare a component that returns an HTML button with the given properties
 const GButton = ({children, variant, disabled, onClick}) => {
 	return <button type="button"
-		variant={variant}
+		className={`GButton ${variant}`}
 		onClick={onClick}
 		disabled={disabled}
 	>{children}</button>;
 };
 
+//Description - appears in the storybook item
 GButton.description = `
 Buttons are used to initialize an action, either in the background or foreground of an experience.
 
@@ -17,6 +21,7 @@ Primary buttons should be used for the principle call to action on the page. Mod
 Small buttons may be used when there is not enough space for a regular sized button. This issue is most found in tables. Small button should have three words or less.
 `;
 
+//This allows for the definition of rules that each prop type has to follow in order to be used properly
 GButton.propTypes = {
 	children: PropTypes.string.isRequired,
 	variant: PropTypes.oneOf(['primary', 'secondary']).isRequired,
@@ -24,6 +29,7 @@ GButton.propTypes = {
 	disabled: PropTypes.bool,
 };
 
+//What properties the component should have when nothing is defined
 GButton.defaultProps = {
 	children: 'I am a button!',
 	variant: 'primary',
