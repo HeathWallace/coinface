4c4e064739b73675eb7137594ecd0e40696026c1
Helen Lawes <helenl.lawes@gmail.com>
2018-02-15 17:09:20 +0000

    Feature/header (#51)
    * adding in new component Header
    
    * adding in settings wheel
    
    * updating class name in header based on feedback

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SettingsWheel from '../SettingsWheel/SettingsWheel';
+
+import './Header.css';
+
+const Header = ({ theme, children, onOpenSettings }) => (
+	<div className={`Header ${theme}`}>
+		<div className="content">{children}</div>
+		<SettingsWheel onClick={onOpenSettings} />
+	</div>
+);
+
+Header.description = `
+A fixed header bar that renders above the transaction list and contains the settings wheel
+`;
+
+Header.defaultProps = {
+	theme: 'light',
+};
+
+Header.propTypes = {
+	/** content to display with the header. */
+	children: PropTypes.string.isRequired,
+
+	/** background theme of the header. */
+	theme: PropTypes.oneOf(['light', 'dark']).isRequired,
+
+	/** function to parse through to SettingsWheel for onClick event */
+	onOpenSettings: PropTypes.func.isRequired,
+
+};
+
+export default Header;
