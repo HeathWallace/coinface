d1e78439cc4197136d2743c94748debb06eeb2ba
Paul Sandwell <paul.sandwell@mirumagency.com>
2018-02-16 11:08:45 +0000

    Code cleanup, removing decorateAction from storybook addons as wasn't needed in the end

--- src/stories.js
+++ src/stories.js
@@ -1,11 +1,10 @@
 import { storiesOf, addDecorator } from '@storybook/react';
-import { action, decorateAction } from '@storybook/addon-actions';
+import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
 
 export {
 	storiesOf,
 	addDecorator,
 	action,
-	decorateAction,
 	withInfo,
 };
