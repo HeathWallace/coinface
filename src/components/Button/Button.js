63a2a2715db9ad5c9c200a7c912befd4a8875b19
George <george.bryant@heathwallace.com>
2018-02-19 18:12:37 +0000

    Added styling to the buttons and removed unnecessary stories

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 
 import './Button.css';
 
-const Button = ({ children, disabled, onClick }) => (
-	<button className="Button"
-		disabled={disabled}
-		type="button"
-		onClick={onClick}
-	>{children}</button>
-);
+const _if = (conditional, ifTrue, ifFalse = '') => conditional ? ifTrue : ifFalse;
 
+//Declare a component that returns an HTML button with the given properties
+const Button = ({children, variant, disabled, onClick}) => {
+	return <button type="button"
+		className={`Button ${variant} ${_if(disabled, 'disabled')}`}
+		onClick={onClick}
+		disabled={disabled}
+	>{children}</button>;
+};
+
+//Description - appears in the storybook item
 Button.description = `
 Buttons are used to initialize an action, either in the background or foreground of an experience.
 
@@ -19,19 +23,19 @@ Primary buttons should be used for the principle call to action on the page. Mod
 Small buttons may be used when there is not enough space for a regular sized button. This issue is most found in tables. Small button should have three words or less.
 `;
 
-Button.defaultProps = {
-	children: 'Click me!',
+//This allows for the definition of rules that each prop type has to follow in order to be used properly
+Button.propTypes = {
+	children: PropTypes.string.isRequired,
+	variant: PropTypes.oneOf(['primary', 'secondary']).isRequired,
+	onClick: PropTypes.func.isRequired,
+	disabled: PropTypes.bool,
 };
 
-Button.propTypes = {
-	/** The text shown inside the button. */
-	children: PropTypes.string.isRequired,
-
-	/** If the button should be disabled and unclickable. */
-	disabled: PropTypes.bool,
-
-	/** A function to be called when the Button is clicked. */
-	onClick: PropTypes.func,
+//What properties the component should have when nothing is defined
+Button.defaultProps = {
+	children: 'I am a button!',
+	variant: 'primary',
+	disabled: false,
 };
 
 export default Button;
--- src/components/Button/Button.story.js
+++ src/components/Button/Button.story.js
@@ -4,17 +4,24 @@ import { storiesOf, action, withInfo } from '../../stories';
 import Button from './Button';
 
 storiesOf('Button')
-
 	.addDecorator((story, context) => withInfo(Button.description)(story)(context))
-
-	.add('with no text', () => (
-		<Button onClick={action('clicked')}/>
+	.add('Primary button', () => (
+		<Button onClick={action('clicked')}
+			variant='primary'
+			disabled={false}>Primary button!</Button>
 	))
-
-	.add('with children', () => (
-		<Button onClick={action('clicked')}>This is the button</Button>
+	.add('Secondary button', () => (
+		<Button onClick={action('clicked')}
+			variant='secondary'
+			disabled={false}>Seondary button!</Button>
 	))
-
-	.add('with some emoji', () => (
-		<Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+	.add('Disabled primary button', () => (
+		<Button onClick={action('clicked')}
+			variant='primary'
+			disabled={true}>Disabled primary button!</Button>
+	))
+	.add('Disabled secondary button', () => (
+		<Button onClick={action('clicked')}
+			variant='secondary'
+			disabled={true}>Disabled secondary button!</Button>
 	));
