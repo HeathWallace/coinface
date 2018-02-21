e4f1779dacd85d49bffabd494f6b43d62bc25244
Dan Hughes <danhughes.dh+github@gmail.com>
2018-02-21 21:01:29 +0000

    Default settings drawer to closed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
 		super(props);
 
 		this.state = {
-			settingsDrawerIsOpen: true,
+			settingsDrawerIsOpen: false,
 		};
 
 		this.toggleSettingsDrawer = this.toggleSettingsDrawer.bind(this);
