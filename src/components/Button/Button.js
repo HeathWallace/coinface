18289df32e53a8f4936ca7181d1db37d8ace0e00
George <george.bryant@heathwallace.com>
2018-02-21 09:02:43 +0000

    Commented the prop types and default prop types

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import './Button.css';
 
+//Some crazy utility making for simpler if statements
 const _if = (conditional, ifTrue, ifFalse = '') => conditional ? ifTrue : ifFalse;
 
 //Declare a component that returns an HTML button with the given properties
@@ -25,16 +26,30 @@ Small buttons may be used when there is not enough space for a regular sized but
 
 //This allows for the definition of rules that each prop type has to follow in order to be used properly
 Button.propTypes = {
+	/** Text that will appear in the button
+	This will default to 'I am a button' if not present*/
 	children: PropTypes.string.isRequired,
+
+	/** This determines the styling and purpose of the buttons
+	Defaults to primary*/
 	variant: PropTypes.oneOf(['primary', 'secondary']).isRequired,
+
+	/** The action performed when the button is clicked
+	There is no default and will throw an error if not present*/
 	onClick: PropTypes.func.isRequired,
+
+	/** Determines if the button is active or nothing
+	Disabled button should have different styling and not be clickable*/
 	disabled: PropTypes.bool,
 };
 
 //What properties the component should have when nothing is defined
 Button.defaultProps = {
+	/** Adds this predetermined string when no child has been added to the button*/
 	children: 'I am a button!',
+	/** Sets the button type to primary if not defined*/
 	variant: 'primary',
+	/** The buttons are enabled unless otherwise specified*/
 	disabled: false,
 };
 
