67baeb4e5b40308cfed4ef5a74a2f91ac6683108
Dan Hughes <danhughes.dh+github@gmail.com>
2018-02-22 17:33:17 +0000

    Sort transactions in container before passing to component (#77)

--- src/containers/TransactionList/TransactionList.js
+++ src/containers/TransactionList/TransactionList.js
@@ -2,10 +2,9 @@ import { connect } from 'react-redux';
 import TransactionsList from '../../components/TransactionsList/TransactionsList';
 
 const mapStateToProps = state => ({
-	transactions: Object.keys(state.transactions).map(hash => ({
-		hash,
-		...state.transactions[hash],
-	})),
+	transactions: Object.keys(state.transactions)
+		.map(hash => ({ hash, ...state.transactions[hash]}))
+		.sort((a, b) => b.timestamp - a.timestamp),
 });
 
 export default connect(mapStateToProps)(TransactionsList);
