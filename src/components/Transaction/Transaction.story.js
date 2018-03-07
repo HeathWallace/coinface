6ae8be0e6d795466f80069586b8a490a3bf08613
Gary Purbrick <gary.purbrick@heathwallace.com>
2018-03-07 15:09:47 +0000

    Finished componentizing the ProgressBar

--- src/components/Transaction/Transaction.story.js
+++ src/components/Transaction/Transaction.story.js
@@ -7,12 +7,45 @@ storiesOf('Transaction')
 
 	.addDecorator((story, context) => withInfo(Transaction.description)(story)(context))
 
-	.add('with junk data', () => (
+	.add('0 Confirmations', () => (
 		<Transaction
 			from={'0xb794f5ea0ba39494ce839613fffba74279579268'}
 			amount={'88.00'}
 			timestamp={1519059931}
-			trust={4}
+			trust={0}
+			hash={'0x126f82dcb2f4a283b4ebe20887deb8fdae8b136bf6c732ab444fd8910b442e3b'}
+			symbol={'ABC'}
+		/>
+	))
+
+	.add('Quarter of confirmations reached', () => (
+		<Transaction
+			from={'0xb794f5ea0ba39494ce839613fffba74279579268'}
+			amount={'88.00'}
+			timestamp={1519059931}
+			trust={25}
+			hash={'0x126f82dcb2f4a283b4ebe20887deb8fdae8b136bf6c732ab444fd8910b442e3b'}
+			symbol={'ABC'}
+		/>
+	))
+
+	.add('Half of confirmations reached', () => (
+		<Transaction
+			from={'0xb794f5ea0ba39494ce839613fffba74279579268'}
+			amount={'88.00'}
+			timestamp={1519059931}
+			trust={50}
+			hash={'0x126f82dcb2f4a283b4ebe20887deb8fdae8b136bf6c732ab444fd8910b442e3b'}
+			symbol={'ABC'}
+		/>
+	))
+
+	.add('Target trust level reached', () => (
+		<Transaction
+			from={'0xb794f5ea0ba39494ce839613fffba74279579268'}
+			amount={'88.00'}
+			timestamp={1519059931}
+			trust={100}
 			hash={'0x126f82dcb2f4a283b4ebe20887deb8fdae8b136bf6c732ab444fd8910b442e3b'}
 			symbol={'ABC'}
 		/>
