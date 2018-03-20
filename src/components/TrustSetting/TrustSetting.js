7f2ea3476a252f3b9472ce339ef206d99142b80e
George <george.bryant@heathwallace.com>
2018-03-20 16:22:56 +0000

    Changed the workings of the component and its CSS

--- src/components/TrustSetting/TrustSetting.js
+++ src/components/TrustSetting/TrustSetting.js
@@ -4,21 +4,51 @@ import inputIdGenerator from '../../utils/inputIdGenerator';
 
 import './TrustSetting.css';
 
-const TrustSetting = ({ id = inputIdGenerator.nextIndex, labelText, minValue, maxValue, value, onChange }) => (
-	<div className='TrustSetting'>
-		<label htmlFor={id}>
-			{labelText}
-		</label>
-		<input
-			type='range'
-			id={id}
-			min={minValue}
-			max={maxValue}
-			value={value}
-			onChange={e => onChange(e.target.value)}
-		/>
-	</div>
-);
+class TrustSetting extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			value: props.value,
+		};
+
+		this.onChange = this.onChange.bind(this);
+	}
+
+	componentWillReceiveProps({ value }) {
+		if (value !== this.state.value) {
+			this.setState({ value });
+		}
+	}
+
+	onChange(event) {
+		const { value: valueString } = event.target;
+		const { onChange } = this.props;
+		const value = parseInt(valueString, 10);
+		this.setState({ value }, () => onChange(value));
+	}
+
+	render() {
+		const { id = inputIdGenerator.nextIndex, labelText, min, max, step } = this.props;
+		const { value } = this.state;
+		return (
+			<div className={`TrustSetting ${value === max ? 'max': ''}`}>
+				<label htmlFor={id}>
+					{labelText}
+				</label>
+				<input
+					type='range'
+					id={id}
+					min={min}
+					max={max}
+					step={step}
+					value={value}
+					onChange={this.onChange}
+				/>
+			</div>
+		);
+	}
+}
 
 TrustSetting.description = `
 A mechanism by which the user can configure a preference between waiting a long time for high confidence or short time with low confidence in transactions validity.`;
@@ -26,6 +56,10 @@ A mechanism by which the user can configure a preference between waiting a long 
 TrustSetting.defaultProps = {
 	/** Text for input label */
 	labelText: 'Trust input',
+
+	value: 0,
+
+	step: 1,
 };
 
 TrustSetting.propTypes = {
@@ -37,16 +71,19 @@ TrustSetting.propTypes = {
 	labelText: PropTypes.string.isRequired,
 
 	/** Minimum value for the slider to decrease to*/
-	minValue: PropTypes.number.isRequired,
+	min: PropTypes.number.isRequired,
 
 	/** Maximum value for the slider to increase to*/
-	maxValue: PropTypes.number.isRequired,
+	max: PropTypes.number.isRequired,
 
 	/** A function to be called when the value of the input is changed. */
 	onChange: PropTypes.func,
 
 	/** Pre-defined value for input, if present */
-	value: PropTypes.number,
+	value: PropTypes.number.isRequired,
+
+	/** Change the step prop to control the granularity of the slider */
+	step: PropTypes.number.isRequired,
 };
 
 export default TrustSetting;
