b67b43a7cd4368420e29373db1b14a73fdc0fc01
Hazlan Rozaimi <hazlan@gmail.com>
2018-03-26 23:53:32 +0800

    Feature/gravatar profile (#137)
    * Add .idea dir in gitignore
    
    * Added gravatar profile
    
    * Added alt text for accesibility

---
+++ src/components/GravatarProfile/GravatarProfile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import md5 from 'md5';
+
+import './GravatarProfile.css';
+
+const GravatarProfile = ({email, gravStyle}) => (
+	<div className='GravatarProfile'>
+		<img src={'https://www.gravatar.com/avatar/' + md5(email.trim().toLowerCase()) + '?d=' + gravStyle} alt="" />
+	</div>
+);
+
+GravatarProfile.description = `
+Used as a visualisation of a customers Gravatar profile picture.
+`;
+
+GravatarProfile.propTypes = {
+	/** email to show the profile of the customer */
+	email: PropTypes.string.isRequired,
+	/** gravatar style to be used to generate default gravatars. Options are mm, identicon, monsterid, wavatar, retro, robohash */
+	gravStyle: PropTypes.string,
+
+};
+
+GravatarProfile.defaultProps = {
+	gravStyle: '',
+};
+
+export default GravatarProfile;
